f4cc57c253e461f5cd2fa4c61a07b4cec5797925
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2023-04-21 15:22:29 -0600

    newest changes: Postgres 14.5, restore qith psql, user novatech, Adempiere ZK version marcalwestf/adempiere-zk-ui:jetty-shw-air-terminal-vertical-1.0.5

--- postgresql/initdb.sh
+++ postgresql/initdb.sh
@@ -3,8 +3,10 @@
 if [ "$( psql -U $POSTGRES_USER -tAc "SELECT 1 FROM pg_roles WHERE rolname='adempiere'" )" != '1' ]
 then
     createuser -U postgres adempiere -dlrs
+    createuser -U novatech novatech -DlRS  # User "novatech", no create DB, no create role, no SuperUser
     psql -U postgres -tAc "alter user adempiere password 'adempiere';"
     createdb -U adempiere adempiere
     #psql -U adempiere -d adempiere < Adempiere/data/Adempiere_pg.dmp
-    pg_restore -U adempiere -d adempiere < /tmp/seed.backup -v
+    #pg_restore -U adempiere -d adempiere < /tmp/seed.backup -v  # Wenn mit pg_dump restauriert wird
+    psql -U adempiere -d adempiere < /tmp/seed.backup  # Wenn mit RUN_DBExport restauriert wird
 fi
